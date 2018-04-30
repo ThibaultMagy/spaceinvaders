b1f54b6c30ac5cf1583b4c1668dbc9382cd2827f
Thibault Magy <magy.thibault@gmail.com>
2018-04-30 16:44:47 +0200

    Correction

--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -14,21 +14,19 @@ public class SpaceInvaders {
 	
 	//METHODES
 	public void positionnerUnNouveauVaisseau(int x, int y) {
-		Vaisseau vaisseau = new Vaisseau (x,y);
+		 this.vaisseau = new Vaisseau (x,y);
 	}
 	
 	//TOSTRING
 	@Override
 	public String toString() {
 		StringBuilder espaceDeJeu = new StringBuilder();
-		for (int x = 0; x < hauteur; x++) {
-			for (int y = 0; y < longueur; y++) {
-				if (vaisseau.occupeLaPosition(x, y)) {
-				     espaceDeJeu.append('V');
-				}
-				else {
+		for (int y = 0; y < hauteur; y++) {
+			for (int x = 0; x < longueur; x++) {
+				if (vaisseau!=null && vaisseau.occupeLaPosition(x, y))
+					espaceDeJeu.append('V');
+				else
 					espaceDeJeu.append('.');
-				}
 			}
 			espaceDeJeu.append('\n');
 		}
--- src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -8,9 +8,16 @@ public class SpaceInvadersTest {
 	@Test
 	public void test_AuDebut_JeuSpaceInvaderEstVide() {
 		SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
-		assertEquals("" + "...............\n" + "...............\n" + "...............\n" + "...............\n"
-				+ "...............\n" + "...............\n" + "...............\n" + "...............\n"
-				+ "...............\n" + "...............\n", spaceinvaders.toString());
+		assertEquals("" + "...............\n" +
+		"...............\n" +
+		"...............\n" +
+		"...............\n"+
+		"...............\n" +
+		"...............\n" +
+		"...............\n" +
+		"...............\n" +
+		"...............\n" +
+		"...............\n", spaceinvaders.toString());
 	}
 	@Test
 	public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
